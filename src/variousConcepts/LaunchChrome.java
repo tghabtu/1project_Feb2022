2e2e6fa95203a4a4f1b530e7888ae553b66430b8
tghabtu <habtu1956abza@gmail.com>
2022-06-25 14:09:13 -0500

    first commit

---
+++ src/variousConcepts/LaunchChrome.java
@@ -0,0 +1,21 @@
+package variousConcepts;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChrome {
+	
+	public static void main (String[]args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\habtu\\Feb22_Selenium-workspace\\1st Selenium\\driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		driver.close();
+		
+		
+	}
+
+}
